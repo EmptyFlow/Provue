5460b8ed72bcc7a8062b0792c3444488885b5587
Romanus <trueromanus@gmail.com>
2021-03-24 22:14:28 +0300

    Added slider control

---
+++ src/states/SliderState.vue
@@ -0,0 +1,92 @@
+<template>
+    <div>
+        <slot :context="this"></slot>
+    </div>
+</template>
+
+<script>
+module.exports = {
+    name: `SliderState`,
+    props: {
+        value: {
+            type: [Number, Array],
+            default: () => 0
+        },
+        isRange: {
+            type: Boolean,
+            default: () => false
+        },
+        minimum: {
+            type: Number,
+            default: () => 0
+        },
+        maximum: {
+            type: Number,
+            default: () => 100
+        },
+        step: {
+            type: Number,
+            default: () => 1
+        },
+        disable: {
+            type: Boolean,
+            default: () => false
+        },
+        validators: {
+            type: Object,
+            default: () => {}
+        },
+        validateHost: {
+            type: Object
+        }
+    },
+    data() {
+        return {
+            position: 0,
+            isValid: false
+        }
+    },
+    created() {
+        this.position = this.value;
+        this.validate();
+    },
+    methods: {
+        validate() {
+            if (!this.validators) return;
+            if (this.validateHost) this.validateHost.clear(this);
+            this.isValid = true;
+
+            for (const [key, value] of Object.entries(this.validators)) {
+                if (!value.check(this.position)) {
+                    this.isValid = false;
+                    if (this.validateHost) this.validateHost.add(this, { rule: key, messages: value.messages });
+                }
+            }
+        },
+        changePosition(position) {
+            if (this.disable) return;
+
+            this.position = position ? parseFloat(position) : 0;
+
+            this.raiseEvents();
+            this.validate();
+        },
+        raiseEvents() {
+            this.$emit(`changed`, this.position); // added for separate v-model events and external events
+            this.$emit(`input`, this.position);
+        }        
+    },
+    watch: {
+        value(newValue) {
+            if (this.isRange && Array.isArray(newValue) && newValue.length !== 2) {
+                console.warn(`SliderState: v-model value is array but don't have two values`);
+                return;
+            }
+            this.position = newValue;
+            
+            this.raiseEvents();
+            this.validate();
+        }
+    }
+};
+</script>
---
+++ src/views/bootstrap/SliderView.vue
@@ -0,0 +1,24 @@
+<template>
+    <input
+        class="form-range"
+        :value="context.position"
+        :min="context.minimum"
+        :max="context.maximum"
+        :step="context.step"
+        @input="context.changePosition($event.target.value)"
+        :disabled="context.disable"
+        type="range"
+    >    
+</template>
+
+<script>
+module.exports = {
+    name: `SliderView`,
+    props: {
+        context: {
+            type: Object,
+            required: true
+        }
+    }
+};
+</script>
